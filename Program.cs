4e77459061e0953dd88df8d7eaed6f533a5cb924
kos kos <lim_job@mail.ru>
2022-04-03 22:28:07 +0300

    обновил 2 задание

--- Program.cs
+++ Program.cs
@@ -5,10 +5,35 @@ class Program
     static void Main(string[] args)
     {
         // Задание №1
-        Console.WriteLine("ЯЮниор");
+        // Console.WriteLine("ЯЮниор");
 
         // Задание №2
-        
+
+        short shortVariable = -10;
+        ushort unsignShortVariale = 10;
+        int integerVariable = -70000;
+        uint unsignIntegerVairable = 3000000000;
+        long longVariable = -100;
+        ulong unsignLongVariable = 200;
+        string text = "heelo world";
+        double doubleVariable = 10;
+        float floatVariable = 10f;
+        Boolean trueFalseSign = true;
+        char znak = '1';
+
+        Console.WriteLine(shortVariable);
+        Console.WriteLine(unsignShortVariale);
+        Console.WriteLine(integerVariable);
+        Console.WriteLine(unsignIntegerVairable);
+        Console.WriteLine(longVariable);
+        Console.WriteLine(unsignLongVariable);
+        Console.WriteLine(text);
+        Console.WriteLine(doubleVariable);
+        Console.WriteLine(trueFalseSign);
+        Console.WriteLine(znak);
+
+
+        /*
         // Объявление без инициализации
         string name;
         string surname;
@@ -37,5 +62,7 @@ class Program
         Console.WriteLine("Дата рождения: " + birthDate);
         Console.WriteLine("Возраст, лет: {0}", ageYears ) ;
         Console.WriteLine("Пол: " + sex);
+
+        */
     }
 }
